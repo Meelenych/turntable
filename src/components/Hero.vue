8cf55378936426079dbc65aa47962619d986bfac
Meelenych <meelenych@gmail.com>
2025-02-17 16:07:46 -0700

    sample data

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,11 @@
 <script setup>
 
+
+fetch('https://api.deezer.com/playlist/4341978/tracks?index=0&limit=10')
+  .then((response) => response.json()) // Return the parsed JSON
+  .then((data) => console.log(data))   // Log the actual data
+  .catch((error) => console.error('Error fetching data:', error)); // Handle errors
+
 </script>
 
 <template>
